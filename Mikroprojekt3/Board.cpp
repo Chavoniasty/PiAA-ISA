13242296715c04bebe43c354b4ba2d199924345f
Chavoniasty <molendadominik@outlook.com>
2023-06-09 22:36:46 +0200

    "fixed syntax and check for winner method"

--- Mikroprojekt3/Board.cpp
+++ Mikroprojekt3/Board.cpp
@@ -1,53 +1,55 @@
 #include "Board.h"
 
-Board::Board(int tempSize){
+Board::Board(int tempSize, int tempPoints) {
     size = tempSize;
-    fields = new char*[tempSize];
-    for(int i = 0; i < size; i++){
+    fields = new char *[tempSize];
+    for (int i = 0; i < size; i++) {
         fields[i] = new char[tempSize];
     }
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             fields[i][j] = ' ';
         }
     }
+    pointsToWin = tempPoints;
 }
 
 void Board::gameStart() const {
-    while(1){
+    while (true) {
         this->displayBoard();
         this->noughtMove();
-        if(checkForWinner('o')){
+        if (checkForWinner('O')) {
             std::cout << "Naughts' won!";
             break;
         }
         this->displayBoard();
         this->crossMove();
-        if(checkForWinner('x')){
+        if (checkForWinner('X')) {
             std::cout << "Crosses' won!";
             break;
         }
     }
 }
 
-void Board::displayBoard() const{
+void Board::displayBoard() const {
     system("cls");
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             std::cout << fields[i][j];
         }
         std::cout << std::endl;
     }
 }
 
-bool Board::checkForWinner(char symbol) const{
+bool Board::checkForWinner(char symbol) const {
     this->displayBoard();
-    for(int i = 0; i < size; i++){
-        int counter = 0;
-        for(int j = 0; j<size; j++){
-            if(fields[i][j] == symbol){
+    int counter = 0;
+    // horizontal check
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (fields[i][j] == symbol) {
                 counter++;
-                if(counter == size){
+                if (counter == pointsToWin) {
                     return true;
                 }
             } else {
@@ -55,12 +57,12 @@ bool Board::checkForWinner(char symbol) const{
             }
         }
     }
-    for(int i = 0; i < size; i++){
-        int counter = 0;
-        for(int j = 0; j<size; j++){
-            if(fields[j][i] == symbol){
+    // vertical check
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (fields[j][i] == symbol) {
                 counter++;
-                if(counter == size){
+                if (counter == pointsToWin) {
                     return true;
                 }
             } else {
@@ -68,59 +70,57 @@ bool Board::checkForWinner(char symbol) const{
             }
         }
     }
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j ++){
-            int counter = 0;
-            if(fields[i][j] == symbol){
-                counter++;
-                if(counter == size) {
-                    return true;
-                }
-            } else {
-                counter = 0;
+    // up left to down right check
+    for (int i = 0; i < size; i++) {
+        if (fields[i][i] == symbol) {
+            counter++;
+            if (counter == pointsToWin) {
+                return true;
             }
+        } else {
+            counter = 0;
         }
     }
-    for(int i = size - 1; i >= 0; i--){
-        for(int j = 0; j < size; j++){
-            int counter = 0;
-            if(fields[i][j] == symbol){
-                counter++;
-                if(counter == size){
-                    return true;
-                }
-            } else {
-                counter = 0;
+    // up right to down left check
+    int j = size - 1;
+    for (int i = 0; i < size; i++) {
+        if (fields[i][j] == symbol) {
+            j--;
+            counter++;
+            if (counter == pointsToWin) {
+                return true;
             }
+        } else {
+            counter = 0;
         }
     }
     return false;
 }
 
-void Board::noughtMove() const{
-    std::cout <<"Insert row number:";
+void Board::noughtMove() const {
+    std::cout << "Insert row number:";
     int rowNum;
     std::cin >> rowNum;
     rowNum--;
-    std::cout <<"Insert column number:";
+    std::cout << "Insert column number:";
     int colNum;
     std::cin >> colNum;
     colNum--;
-    if(fields[rowNum][colNum] == ' '){
-        fields[rowNum][colNum] = 'o';
+    if (fields[rowNum][colNum] == ' ') {
+        fields[rowNum][colNum] = 'O';
     }
 }
 
 void Board::crossMove() const {
-    std::cout <<"Insert row number:";
+    std::cout << "Insert row number:";
     int rowNum;
     std::cin >> rowNum;
     rowNum--;
-    std::cout <<"Insert column number:";
+    std::cout << "Insert column number:";
     int colNum;
     std::cin >> colNum;
     colNum--;
-    if(fields[rowNum][colNum] == ' '){
-        fields[rowNum][colNum] = 'x';
+    if (fields[rowNum][colNum] == ' ') {
+        fields[rowNum][colNum] = 'X';
     }
 }
--- Mikroprojekt3/Board.h
+++ Mikroprojekt3/Board.h
@@ -1,17 +1,24 @@
 #ifndef MIKROPROJEKT3_BOARD_H
 #define MIKROPROJEKT3_BOARD_H
+
 #include <iostream>
 
 class Board {
 public:
     int size;
     char **fields;
+    int pointsToWin;
 
-    explicit Board(int tempSize);
+    explicit Board(int tempSize, int tempPoints);
+
     void displayBoard() const;
+
     void gameStart() const;
+
     [[nodiscard]] bool checkForWinner(char symbol) const;
+
     void noughtMove() const;
+
     void crossMove() const;
 };
 
